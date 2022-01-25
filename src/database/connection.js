e37ac1ea58de94e5c040c7d274c96ba2213b89f8
José Miranda <josemiranda989@gmail.com>
2022-01-25 17:25:53 -0300

    Config db

---
+++ src/database/connection.js
@@ -0,0 +1,23 @@
+import sql from "mssql";
+
+const dbSettings = {
+  user: "jose",
+  password: "jose1234",
+  server: "localhost",
+  database: "Cofaral",
+  options: {
+    encrypt: true,
+    trustServerCertificate: true
+  }
+};
+
+async function getConnection() {
+  try {
+    const pool = await sql.connect(dbSettings)
+    return pool
+  } catch (err) {
+    console.error(error);
+  }
+}
+
+getConnection()
